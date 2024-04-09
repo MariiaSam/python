cd3a2ce162bb611ea240e113bdcad9116a9b9c82
MariiaSam <kontragent@ukr.net>
2024-04-09 17:23:36 +0300

    +

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-def sum_nums(a, b):
-    sum = a - b
-    return sum
+# def sum_nums(a, b):
+#     sum = a - b
+#     return sum
 
 
-first_sum = sum_nums(5, 4)
-print(first_sum)
+# first_sum = sum_nums(5, 4)
+# print(first_sum)
+
+
+# print(input("Enter your name: "))
+
+
+import datetime
+
+print(datetime.MAXYEAR)
