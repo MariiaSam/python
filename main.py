fd3e8f36fc8666d8bd4f28002bf4ec3b0018d09f
MariiaSam <kontragent@ukr.net>
2024-04-10 22:42:24 +0300

    add

--- main.py
+++ main.py
@@ -22,3 +22,29 @@
 
 # result = sum_nums(5, 3)
 # print(result)
+
+my_number = 10
+print(my_number)
+# кожне число - це ексз класу int
+
+# цілі числа, строки, логічні типи, списки, словники
+
+my_numb = 34
+print(my_numb)
+# цілі числа,
+
+my_name = 'Mariia'
+print(my_name)
+# строки
+
+my_type = False
+print(my_type)
+# логічні типи,
+
+my_list = [1, "Mariia", True]
+print(my_list)
+# списки
+
+my_dictionary = {"name": 'Mariia', "son": "Lev"}
+print(my_dictionary)
+# словники
