14350c956ea598e58160fc62a7a2d04da61bdc0a
MariiaSam <kontragent@ukr.net>
2024-05-01 13:50:02 +0300

    +

--- main.py
+++ main.py
@@ -751,3 +751,14 @@ users_inputs = [True, 1, 'hi', False, 6]
 
 # print(id(my_cars) == id(copied_cars)) # False
 
+# ====================== 
+
+my_nums = [10, 30, 688, 123]
+
+print(type(my_nums)) # <class 'list'>
+
+
+print(dir(my_nums)) # <class 'list'>
+# ['__add__', '__class__', '__class_getitem__', '__contains__', '__delattr__', '__delitem__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__getitem__', '__getstate__', '__gt__', '__hash__', '__iadd__', '__imul__', '__init__', '__init_subclass__', '__iter__', '__le__', '__len__', '__lt__', '__mul__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__reversed__', '__rmul__', '__setattr__', '__setitem__', '__sizeof__', '__str__', '__subclasshook__', 'append', 'clear', 'copy', 'count', 'extend', 'index', 'insert', 'pop', 'remove', 'reverse', 'sort']
+
+
