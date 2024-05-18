34129e7e47d1547f374c70c6a033f7fef381e6cb
MariiaSam <kontragent@ukr.net>
2024-05-18 19:42:57 +0300

    +

--- basic.py
+++ basic.py
@@ -1,9 +1,35 @@
 
 
 
-float1 = 5.0
-float2 = -1.25
+# float1 = 5.0
+# float2 = -1.25
 
-float3 = 587643291.001
+# float3 = 587643291.001
 
-print(float)
+# print(float)
+
+# menu = ["wraps", "sandwiches", "soup", "salad"]
+# for i in menu:
+#     print(i) # wraps
+# sandwiches
+# soup
+# salad
+
+#================================
+
+# age = int(input("How old are you now? "))
+# print("How old will you be next year?")
+# print(age + 1)
+
+#  my_lucky_number = 7
+# guess = int(input(“Guess my lucky number! I think it is: ”))
+# while my_lucky_number != guess:
+#      guess = int(input(“Oops! Not it. Try again: ”))
+# print(“Congrats! You guessed it.”)
+
+
+my_lucky_number = 7
+guess = int(input('Guess my lucky number! I think it is: '))
+while my_lucky_number != guess:
+     guess = int(input('Oops! Not it. Try again: '))
+print('Congrats! You guessed it.')
