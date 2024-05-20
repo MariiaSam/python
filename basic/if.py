41726c035fba3c3cdb7956420343f5edc8c554d5
MariiaSam <kontragent@ukr.net>
2024-05-20 12:03:08 +0300

    +

---
+++ basic/if.py
@@ -0,0 +1,6 @@
+rate = 1.68
+value_day = 3
+value_night = 3
+payment = (rate * value_day) + (rate * value_night)/2
+print(payment)
+
---
+++ hw/hw.py
@@ -0,0 +1,19 @@
+length = 2.75
+width = 1.75
+area = length * width
+show = f"With width {width} and length {length} of the room, its area is equal to {area}"
+
+print(show)
+
+
+length = input(float(10))
+width = input(float(10))
+area = length * width
+print(area)
+
+my_list = []
+my_list[0] = 2024
+my_list[1] = 'Python'
+my_list[2] = 3.12
+
+print(my_list)
