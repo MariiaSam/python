4beb02a913b97ad6f2f6210e90791130283d5d35
MariiaSam <kontragent@ukr.net>
2024-05-22 22:18:08 +0300

    +

--- moduls/1.py
+++ moduls/1.py
@@ -1,7 +1,28 @@
 message = "Hello world!"
 print(message[0])
+#===========================
 
 name = "Oleg"
 hello_string = f"Hello, {name}!"
 print(hello_string)
+#===========================
 
+side_a = 10
+side_b = 5
+
+hypotenuse = (side_a**2 + side_b**2)**0.5
+print(hypotenuse) # 11.180339887498949
+
+S = side_a * side_b / 2
+print(S) #25.0
+
+#===========================
+n = 5000
+
+hours = n // (60 * 60)
+minutes = (n - hours * 60 * 60) // 60
+seconds = n - hours * 60 * 60 - minutes * 60
+
+print(hours) # 1
+print(minutes) # 23
+print(seconds) # 20
