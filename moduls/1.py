3e06044342d36204274ab6a8ac4aa21cea7baa17
MariiaSam <kontragent@ukr.net>
2024-05-24 17:29:05 +0300

    +

--- moduls/1.py
+++ moduls/1.py
@@ -1,3 +1,23 @@
+# ТИПИ ДАНИХ
+
+str = "Mariia" # строка
+
+# Списки (Lists)
+my_list = list()
+empty_list = []
+my_list = [1, 2, 3, 4, 5]
+my_list = [1, "Hello", 3.14]
+my_list.append(4)
+my_list.remove("Hello")
+
+
+
+# Кортежі (Tuples)
+# Словники (Dictionaries)
+# Множини (Sets)
+# Заморожені множини (Frozen Sets)
+
+
 message = "Hello world!"
 print(message[0])
 #===========================
@@ -26,3 +46,46 @@ seconds = n - hours * 60 * 60 - minutes * 60
 print(hours) # 1
 print(minutes) # 23
 print(seconds) # 20
+
+#+==========================
+
+x = 12
+print(f"Значення x: {x}") # Значення x: 12
+
+# a = input("Рядок запрошення: ")
+
+age = input("How old are you? ")
+age = int(age)
+
+pi = float('3.14')
+
+#==========================================
+a = float(input("Введіть сторону квадрата a: "))
+P = 4 * a
+print(f"Периметр квадрата дорівнює {P}")
+
+
+#==========================================
+
+# Встановлюємо ціни на продукти
+price_per_croissant = 1.04
+price_per_glass = 0.34
+price_per_coffee_pack = 4.42
+
+# Кількість кожного продукту
+num_croissants = int(input("Введіть кількість круасанів: "))
+num_glasses = int(input("Введіть кількість склянок: "))
+num_coffee_packs = int(input("Введіть кількість упаковок кави: "))
+
+# Обчислення загальної вартості
+total_cost = num_croissants * price_per_croissant + num_glasses * price_per_glass + num_coffee_packs * price_per_coffee_pack
+
+# Визначаємо кількість повних доларів і центів
+total_dollars = int(total_cost)
+total_cents = int(total_cost * 100)
+
+# Вивід результату
+print(f"Загальна вартість у повних доларах: {total_dollars} доларів")
+print(f"Загальна вартість у центах: {total_cents} центів")
+
+#==========================================
