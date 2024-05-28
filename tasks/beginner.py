3af5050ffd3df585b73518d82a98c5c286742b5d
MariiaSam <kontragent@ukr.net>
2024-05-28 19:23:27 +0300

    add task

--- tasks/beginner.py
+++ tasks/beginner.py
@@ -13,26 +13,26 @@
 # 36
 # 0.125
 
-first_numb = int(2)
-second_numb = int(3)
-third_numb = int(6)
+# first_numb = int(2)
+# second_numb = int(3)
+# third_numb = int(6)
 
-print(first_numb)
-print(second_numb)
-print(third_numb)
+# print(first_numb)
+# print(second_numb)
+# print(third_numb)
 
-total_sum = first_numb + second_numb + third_numb
+# total_sum = first_numb + second_numb + third_numb
 
-print(total_sum) # 11
+# print(total_sum) # 11
 
-total_mult = first_numb * second_numb * third_numb
-print(total_mult) # 36
+# total_mult = first_numb * second_numb * third_numb
+# print(total_mult) # 36
 
-diff = (second_numb - third_numb)
-total_diff = first_numb ** diff
-print(total_diff)
+# diff = (second_numb - third_numb)
+# total_diff = first_numb ** diff
+# print(total_diff)
 
-#==================================================
+# ==================================================
 
 # 2. Напишіть програму, яка приймає ціле число n і обчислює значення виразу n + nn + nnn.
 
@@ -48,21 +48,21 @@ print(total_diff)
 # 369
 # 123
 
-first_numb = int(5)
-second_numb = int(3)
-third_numb = int(1)
+# first_numb = int(5)
+# second_numb = int(3)
+# third_numb = int(1)
 
 # 5 + 55 + 555 = 615
 
-nn_numb = str(first_numb)+str(first_numb)
-nnn_numb = str(first_numb)+str(first_numb) + str(first_numb)
-print(nn_numb) # 55
-print(nnn_numb) #555
+# nn_numb = str(first_numb)+str(first_numb)
+# nnn_numb = str(first_numb)+str(first_numb) + str(first_numb)
+# print(nn_numb) # 55
+# print(nnn_numb) #555
 
-result = first_numb + int(nn_numb) + int(nnn_numb)
-print(result) # 615
+# result = first_numb + int(nn_numb) + int(nnn_numb)
+# print(result) # 615
 
-#==================================================
+# ==================================================
 
 # 3. Напишіть програму, яка отримує такі дані: ім’я, вік, хобі, введені з клавіатури (вводяться на окремих рядках), і друкує на екрані одним повідомленням повну інформацію на основі введених даних.
 
@@ -75,14 +75,14 @@ print(result) # 615
 
 # My name is Lord Voldemort. I am 72 and my hobby is Magic.
 
-name = "Lord Voldemort"
-age = 72
-hobby = "Magic"
+# name = "Lord Voldemort"
+# age = 72
+# hobby = "Magic"
 
-str = "My name is " + name + ". I am " + str(int(age)) + " and my " + "hobby is " + hobby 
-print(str)
+# str = "My name is " + name + ". I am " + str(int(age)) + " and my " + "hobby is " + hobby
+# print(str)
 
-#==================================================
+# ==================================================
 
 
 # 4. Напишіть програму, яка зчитує довжину основи та висоту прямокутного трикутника (цілі числа), обчислює площу і друкує її значення на екрані у відформатованому вигляді (два символи після десяткової крапки). Кожен параметр вводиться на окремому рядку.
@@ -95,16 +95,16 @@ print(str)
 
 # 6.00
 
-length = int(3)
-print(length)
+# length = int(3)
+# print(length)
 
-heigth = int(4)
-print(heigth)
+# heigth = int(4)
+# print(heigth)
 
-total = 0.50 * length * heigth
-print(f"{total:.2f}") 
+# total = 0.50 * length * heigth
+# print(f"{total:.2f}")
 
-#==================================================
+# ==================================================
 
 
 # Встановлюємо ціни на продукти
@@ -131,8 +131,8 @@ print(f"{total:.2f}")
 # print(f"Загальна вартість у центах: {total_cents} центів")
 
 
-#==================================================
-#5. Напишіть програму, яка зчитує ціле число, і друкує попереднє та наступне числа відносно введеного.
+# ==================================================
+# 5. Напишіть програму, яка зчитує ціле число, і друкує попереднє та наступне числа відносно введеного.
 
 # Вхідні дані:
 
@@ -142,13 +142,13 @@ print(f"{total:.2f}")
 # The next number for the number 179 is 180.
 # The previous number for the number 179 is 178.
 
-num = 179
-print(type(num))
+# num = 179
+# print(type(num))
 
-print(f"The next number for the number {num} is {num + 1}.") # The next number for the number 179 is 180.
-print(f"The previous number for the number {num} is {num - 1}.") # The previous number for the number 179 is 178.
+# print(f"The next number for the number {num} is {num + 1}.") # The next number for the number 179 is 180.
+# print(f"The previous number for the number {num} is {num - 1}.") # The previous number for the number 179 is 178.
 
-#==================================================
+# ==================================================
 # 6. Вводиться ціле додатне число. Надрукуйте останню цифру числа.
 
 # Вхідні дані:
@@ -162,4 +162,93 @@ print(f"The previous number for the number {num} is {num - 1}.") # The previous 
 # 0
 # 1
 
-num_1 = 179
+# def last_digit(n):
+#     return n % 10
+
+# print(last_digit(179))  # 9
+# print(last_digit(40))   # 0
+# print(last_digit(101))  # 1
+
+
+# ==================================================
+# 7.1. Виконай попереднє завдання за допомогою метода format.
+# Зразок виконання:
+
+# f_name = input('Введіть ім\'я: ')
+
+# s_name = input('Введіть прізвище: ')
+
+# print('Мене звуть {} {}. Я з України.'.format(f_name, s_name))
+
+# f_name = input('Введіть ім\'я: ')
+
+# s_name = input('Введіть прізвище: ')
+
+# print(f'Мене звуть {f_name} {s_name}. Я з України.')
+
+# ==================================================
+# 7.2. Перетвори речення у багаторядкову стрічку та виведи його:
+# "Інформатика — теоретична та прикладна дисципліна, що вивчає структуру і загальні властивості інформації, а також методи і (технічні) засоби її створення, перетворення, зберігання, передачі та використання в різних галузях людської діяльності. "
+
+# sentence = '''Інформатика — теоретична та прикладна дисципліна, що вивчає структуру і загальні властивості інформації, а також методи і (технічні) засоби її створення, перетворення, зберігання, передачі та використання в різних галузях людської діяльності.'''
+# print(sentence)
+
+# ==================================================
+# 7.3. Склади програму, яка виводить на екран словосполучення "Computer Science" 10 разів.
+
+# print('Computer Science'  * 10)
+
+# ==================================================
+# 7.4. Використовуючи функцію split, розбий стрічку "Computer Science" через пробіли. В результаті повинен вийти список слів.
+
+# sentence = 'Computer Sciense'
+
+# print(sentence.split(" "))
+
+# ==================================================
+# 8.1 Ввести з клавіатури слово. Перетворити його в список.
+# a = input("Введи слово: ")
+
+# print(list(a)) # ['M', 'a', 'r', 'i', 'i', 'a']
+
+# word_list = [a]
+# print(word_list) # ['Mariia']
+
+# ==================================================
+# 8.2. Склади список улюблених хобі і дай йому назву змінної hobbies. Склади список своїх улюблених страв і назви цю змінну food. Об’єднай два списки й назви результат favourite. Виведи цю змінну.
+
+# hobbies = ['read', 'code', 'swim']
+# food = ['water', 'avocado', 'meat']
+
+# favourite = hobbies + food
+
+# print(favourite) # ['read', 'code', 'swim', 'water', 'avocado', 'meat']
+
+# ==================================================
+# 8.3. 3. Створи список, що складається з усіх цифр у порядку зростання. Використай функцію, що змінює порядок елементів списку на зворотній та виведи отриманий список. Обчисли кількість елементів у списку.
+
+# num_list = [1, 2, 3, 4, 7, 9, 13, 14, 16]
+# num_list.reverse()
+# print(num_list) # [16, 14, 13, 9, 7, 4, 3, 2, 1]
+
+# print(len(num_list)) # 9
+
+# ==================================================
+# 8.4. Склади список із букв українського алфавіту. За допомогою функції del видали усі голосні літери.
+# alphabet = ['а', 'б', 'в', 'г', 'ґ', 'д', 'е', 'є', 'ж', 'з', 'и', 'і', 'ї', 'й', 'к', 'л',
+#             'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ',  'ь',  'ю', 'я']
+
+# vowels = ['а', 'е', 'є', 'и', 'і', 'ї', 'о', 'у', 'ю', 'я']
+
+# for vowel in vowels:
+#     while vowel in alphabet:
+#         del alphabet[alphabet.index(vowel)]
+
+# print(alphabet) # ['б', 'в', 'г', 'ґ', 'д', 'ж', 'з', 'й', 'к', 'л', 'м', 'н', 'п', 'р', 'с', 'т', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ь']
+
+# ==================================================
+# 8.5. Створи список із п'яти випадкових чисел з діапазону від 0 до 100. Для цього скористайся функцією sample з модуля random.
+
+import random
+
+print(random.sample(range(100), 5))  # [56, 54, 26, 90, 66]
