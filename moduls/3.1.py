15c3b6a2934718a996755dda924602523a53afd7
MariiaSam <kontragent@ukr.net>
2024-06-12 23:00:13 +0300

    +

--- moduls/3.1.py
+++ moduls/3.1.py
@@ -28,13 +28,22 @@
 # datetime.datetime.combine(date_object, time_object)
 
 # ==================================================
+# import datetime
+
+# # Створення об'єктів date і time
+# date_part = datetime.date(2023, 12, 14)
+# time_part = datetime.time(12, 30, 15)
+
+# # Комбінування дати і часу в один об'єкт datetime
+# combined_datetime = datetime.datetime.combine(date_part, time_part)
+
+# print(combined_datetime)  # Виведе "2023-12-14 12:30:15"
+
+# ==================================================
+
 import datetime
 
-# Створення об'єктів date і time
-date_part = datetime.date(2023, 12, 14)
-time_part = datetime.time(12, 30, 15)
+# Створення об'єкта datetime з конкретною датою
+specific_date = datetime.datetime(year=2020, month=1, day=7)
 
-# Комбінування дати і часу в один об'єкт datetime
-combined_datetime = datetime.datetime.combine(date_part, time_part)
-
-print(combined_datetime)  # Виведе "2023-12-14 12:30:15"
+print(specific_date)  # Виведе "2020-01-07 00:00:00"
