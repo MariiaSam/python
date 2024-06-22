8fb5278e9b1b5dd2dd335d2d0d43f7bffe8591a7
MariiaSam <kontragent@ukr.net>
2024-06-22 18:38:33 +0300

    +

--- practice/1.py
+++ practice/1.py
@@ -122,3 +122,16 @@
 #         days +=1
 #     return days
 # print(smoke()) # 18
+
+# ======================
+n = int(input())
+
+if n % 2 == 0 and range(2, 5):
+    print("Not Weird")
+elif n % 2 == 0 and range(6, 20):
+    print("Weird")
+elif n % 2 == 0 and n > 20:
+    print("Not Weird")
+else:
+    print("Weird")
+
