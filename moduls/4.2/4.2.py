3a60594dd83242866b50b47ed8e7db8768a02148
MariiaSam <kontragent@ukr.net>
2024-06-25 18:32:03 +0300

    modul

---
+++ moduls/4.2/4.2.py
@@ -0,0 +1,23 @@
+# import math
+
+# sin_pi = math.sin(math.pi)
+# print(sin_pi) # 1.2246467991473532e-16
+
+# ====================================
+
+# from math import pi, sin
+
+# sin_pi = sin(pi)
+
+
+
+# ====================================
+# def say_hello(name):
+#     print(f'Hello, {name}')
+
+# print("You imported hello.py")
+# say_hello('user')
+
+
+
+
