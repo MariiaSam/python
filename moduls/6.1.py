7a5273ec037ba2ffa567c6af8985af7bd8d9c55b
MariiaSam <kontragent@ukr.net>
2024-07-14 17:22:23 +0300

    add

--- moduls/6.1.py
+++ moduls/6.1.py
@@ -3,51 +3,51 @@
 
 # p = Person() # об'єкт-екземпляр класу
 
-#========================================
-class User:
-    name = 'Anonymous'
-    age = 15
+# #========================================
+# class User:
+#     name = 'Anonymous'
+#     age = 15
 
-user1 = User()
-print(user1.name)
-print(user1.age)
+# user1 = User()
+# print(user1.name)
+# print(user1.age)
 
-user2 = User()
-user2.name = "John"
-user2.age = 90
+# user2 = User()
+# user2.name = "John"
+# user2.age = 90
 
-print(user2.name)
-print(user2.age)
+# print(user2.name)
+# print(user2.age)
 
 
 #++++++++++++++++++++++++++++++++
 
-class User:
-    name = None # властивості, атрибути
-    age = None
-    phone = None
+# class User:
+#     name = None # властивості, атрибути
+#     age = None
+#     phone = None
 
-    def greeting(self ):   # метод клaсу
-        return f"I'm {self.name}"
+#     def greeting(self ):   # метод клaсу
+#         return f"I'm {self.name}"
 
-user_1 = User() # обʼєкт класу User
-user_1.name = "Mariia"
-user_1.age = 34
-user_1.phone = '089898' # викликаємо атрибути
-print(user_1) # <__main__.User object at 0x104458d10>
-print(user_1.name) # Mariia
-print(user_1.greeting())# викликаємо метод класу I'm Mariia
+# user_1 = User() # обʼєкт класу User
+# user_1.name = "Mariia"
+# user_1.age = 34
+# user_1.phone = '089898' # викликаємо атрибути
+# print(user_1) # <__main__.User object at 0x104458d10>
+# print(user_1.name) # Mariia
+# print(user_1.greeting())# викликаємо метод класу I'm Mariia
 
 #++++++++++++++++++++++++++++++++   Наслідування
-class Person:
-    name = None
+# class Person:
+#     name = None
 
-    def greeting(self):
-        print(f"I'm {self.name}")
+#     def greeting(self):
+#         print(f"I'm {self.name}")
     
-class Developer(Person):
-    def do_job(self):
-        print(f"I'm {self.name} writting code at that moment")
+# class Developer(Person):
+#     def do_job(self):
+#         print(f"I'm {self.name} writting code at that moment")
     
     # створимо обʼєкт класу Джуніор
 
@@ -57,18 +57,44 @@ class Developer(Person):
 # junior.do_job()
 
 
-class Manager(Person):
-    def manage(self):
-        print("Deadline is coming")
+# class Manager(Person):
+#     def manage(self):
+#         print("Deadline is coming")
 
-class TeamLead(Developer, Manager):
-    def manage(self):
-        print("I'm TeamLead")
-        return super().manage()
+# class TeamLead(Developer, Manager):
+#     def manage(self):
+#         print("I'm TeamLead")
+#         return super().manage()
 
-teamLead = TeamLead()
-teamLead.name = 'Lev'
-teamLead.greeting()
-teamLead.do_job()
-teamLead.manage()
+# teamLead = TeamLead()
+# teamLead.name = 'Lev'
+# teamLead.greeting()
+# teamLead.do_job()
+# teamLead.manage()
 
+
+# 
+class Person:
+    def __init__(self, name: str, age: int, is_active: bool):
+        self.name = name
+        self.age = age
+        self._is_active = is_active
+
+    def greeting(self):
+        return f"Hi {self.name}"
+    
+    def is_active(self):
+        return self._is_active
+
+    def set_active(self, active: bool):
+        self._is_active = active
+
+p = Person("Boris", 34, True)
+print(p.name, p.age, p.is_active())
+print(p.greeting())
+
+
+'''
+Boris 34 True
+Hi Boris
+'''
--- moduls/6.2.py
+++ moduls/6.2.py
@@ -1,4 +1,4 @@
-from collections import UserDict
+'''from collections import UserDict
 
 class MyDictionary(UserDict):
     # Приклад додавання нового методу
@@ -9,11 +9,11 @@ class MyDictionary(UserDict):
 my_dict = MyDictionary({'a': 1, 'b': 2})
 my_dict.add_key('c', 3)
 print(my_dict)
-
+'''
 
 # =====================================
 
-from collections import UserList
+'''from collections import UserList
 from random import randint
 
 class ExperimentsResults(UserList):
@@ -34,10 +34,10 @@ print(result)
 print(result.positive_values())
 print(result.negative_values())
 print(result.data)
-
+'''
 
 #==============================
-from collections import UserDict
+'''from collections import UserDict
 
 
 class DualDict(UserDict):
@@ -70,3 +70,63 @@ print(dual)
 print(dual.get_by_value(2) == 1)
 dual.set_by_value(4, 8)
 print(dual)
+'''
+
+from collections import UserDict
+
+class MyDictionary(UserDict):
+    # Приклад додавання нового методу
+    def add_key(self, key, value):
+        self.data[key] = value
+
+# Створення екземпляра власного класу
+my_dict = MyDictionary({'a': 1, 'b': 2})
+my_dict.add_key('c', 3)
+print(my_dict)
+
+'''
+{'a': 1, 'b': 2, 'c': 3}
+'''
+
+from collections import UserDict
+
+contacts = [
+    {
+        "name": "Allen Raymond",
+        "email": "nulla.ante@vestibul.co.uk",
+        "phone": "(992) 914-3792",
+        "favorite": False,
+    },
+    {
+        "name": "Chaim Lewis",
+        "email": "dui.in@egetlacus.ca",
+        "phone": "(294) 840-6685",
+        "favorite": False,
+    },
+    {
+        "name": "Kennedy Lane",
+        "email": "mattis.Cras@nonenimMauris.net",
+        "phone": "(542) 451-7038",
+        "favorite": True,
+    }
+]
+
+class Customer(UserDict):
+    def phone_info(self):
+        return f"{self.get('name')}: {self.get('phone')}"
+
+    def email_info(self):
+        return f"{self.get('name')}: {self.get('email')}"
+
+if __name__ == "__main__":
+    customers = [Customer(el) for el in contacts]
+
+    print("---------------------------")
+
+    for customer in customers:
+        print(customer.phone_info())
+
+    print("---------------------------")
+
+    for customer in customers:
+        print(customer.email_info())
