971a9d102a716855cb85cb395d68e7c2200318f9
MariiaSam <kontragent@ukr.net>
2024-07-16 17:26:50 +0300

    task_bank_oop
    put
    get
    check

---
+++ tasks/bank_oop/bank_oop.py
@@ -0,0 +1,47 @@
+'''
+Напишіть клас Bank для опису простих операції з вашим банківським рахунком: 
+покласти на рахунок, 
+зняти з рахунку, 
+переглянути рахунок.
+
+ При створенні екземпляру класу, екземпляр отримує атрибут __balance з певним значенням. Клас повинен містити методи для додавання коштів на рахунок і знімання з рахунку, за умови, що на рахунку достатньо коштів.
+
+
+'''
+
+class Bank:
+
+
+    def __init__(self, initial_balance = 0 ) -> None:
+        self.__balance = initial_balance
+    
+    def put_money(self, amount):
+        if amount > 0:
+             self.__balance += amount
+             print(f"Ви успішно поклали {amount:.2f} грн на рахунок. Ваш новий баланс: {self.__balance:.2f} грн.")
+        else:
+             print("Неможливо покласти негативну суму.")
+
+    def get_money(self, amount):
+        if amount > 0 and amount <= self.__balance:
+            self.__balance -= amount
+            print(f"Ви зняли {amount:.2f} грн з рахунку. Ваш новий баланс: {self.__balance:.2f} грн.")
+        elif amount <= 0:
+            print("Неможливо зняти негативну суму.")
+        else:
+            print(f"Недостатньо коштів на рахунку. Максимальна сума зняття: {self.__balance:.2f} грн.")
+        
+    def check_money(self):
+        print(f"Ваш баланс: {self.__balance:.2f} грн.")
+
+bank_account = Bank(1000)
+
+bank_account.put_money(500)
+
+bank_account.get_money(200)
+
+bank_account.check_balance()
+
+bank_account.get_money(1500)
+
+bank_account.put_money(-100)
--- tasks/shop.oop/import_shop.py
+++ tasks/shop.oop/import_shop.py
@@ -3,3 +3,9 @@ from shop import Shop
 all_store = Shop("All-in-One", "department store")
 
 all_store.describe_shop()
+
+'''
+Store name is All-in-One
+Store type is department store
+quantity of goods 0
+'''
