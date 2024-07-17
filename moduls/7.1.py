5154932c1f9a0b116d08c2cf09376d111bfaa02e
MariiaSam <kontragent@ukr.net>
2024-07-17 21:08:39 +0300

    add

--- moduls/7.1.py
+++ moduls/7.1.py
@@ -57,4 +57,60 @@ print(new_point) # Point (x=3, y=9)
 
 #===================================================
 
+class SimpleDict:
+    def __init__(self) -> None:
+            self.__data = {}
+
+    def __getitem__(self, key): # дозволяє отримати значення за ключем
+        return self.__data.get(key, "Key not found")
     
+    def __setitem__(self, key, value): #  встановити нове значення для ключа.
+        self.__data[key] = value
+
+# Використання класу
+simple_dict = SimpleDict()
+simple_dict['name'] = 'Boris'
+print(simple_dict['name'])  
+print(simple_dict['age'])  
+
+
+'''
+Boris
+Key not found
+'''
+
+class BoundedList:
+    def __init__(self, min_value: int, max_value: int):
+        self.min_value = min_value
+        self.max_value = max_value
+        self.__data = []
+
+    def __getitem__(self, index: int):
+        return self.__data[index]
+
+    def __setitem__(self, index: int, value: int):
+        if not (self.min_value <= value <= self.max_value):
+            raise ValueError(f"Value {value} must be between {self.min_value} and {self.max_value}")
+        if index >= len(self.__data):
+            # Додати новий елемент, якщо індекс виходить за межі
+            self.__data.append(value)
+        else:
+            # Замінити існуючий елемент
+            self.__data[index] = value
+
+    def __repr__(self):
+        return f"BoundedList({self.max_value}, {self.min_value})"
+
+    def __str__(self):
+        return str(self.__data)
+
+if __name__ == '__main__':
+    temperatures = BoundedList(18, 26)
+
+    for i, el in enumerate([20, 22, 25, 27]):
+        try:
+            temperatures[i] = el
+        except ValueError as e:
+            print(e)
+
+    print(temperatures)
