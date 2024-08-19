72881c58e929c543ad1f4f332b5c4a5357e3049b
MariiaSam <kontragent@ukr.net>
2024-08-19 21:19:09 +0300

    add task

--- tasks/dict.py
+++ tasks/dict.py
@@ -9,7 +9,7 @@
 # # {'a': 'm', 'b': 122, 'c': 122, 'd': 122}
 
 # 1
-import re 
+import re
 
 exemple_text = """Little girl, little girl Why are you crying? Inside your restless soul your heart is dying Little one, little one Your soul is purging Of love and razor blades Your blood is surging Runaway From the river to the street And find yourself with your face in the gutter Your a stray for the salvation army There is no place like home When you got no place to go Little girl, little girl Your life is calling The charlatans and saints of your abandon Little one little one The sky is falling The lifeboat of deception is now sailing In the wake all the way No rhyme or reason Your bloodshot eyes Will show your heart of treason Little girl little girl You dirty liar You're just a junkie Preaching to the choir Runaway To your lost tranquility And find yourself with your face in the gutter Your a stray for the dregs of humanity There is no place like home When you got no place to go The traces of blood Always follow you home Like the Mascara tears From your getaway Your walking with blisters And running with shears So unholy. Sister of grace. Runaway From the river to the street And find yourself with your face in the gutter You're a stray for the salvation army There is no place like home When you got no place to go"""
 
@@ -22,7 +22,7 @@ for word in words:
     #     exemple_dict[word] = 1
     # else:
     #     exemple_dict[word] += exemple_dict[word] + 1
-    
+
     exemple_dict[word] = exemple_dict.get(word, 0) + 1
 
 new_dict = {}
@@ -31,7 +31,7 @@ for key, value in exemple_dict.items():
     new_dict[value] = new_dict.get(value, []) + [key, ]
 
 for key in sorted(new_dict, reverse=True):
-     print(f" {key} : {new_dict[key]}")
+    print(f" {key} : {new_dict[key]}")
 
 
 '''
@@ -49,3 +49,29 @@ for key in sorted(new_dict, reverse=True):
 
 '''
 
+''''
+Створіть словник зі списками добрих справ на сьогодні і на завтра. Надрукуйте із словника добрі справи, які плануєш зробити сьогодні і взавтра.
+
+Вхідні дані:
+
+Немає
+Вихідні дані:
+
+today:
+Make a compliment to a friend
+Call your grandparents
+Embrace parents
+tomorrow:
+Feed the birds in the park
+Give unnecessary things to those who need them
+Smile
+
+'''
+good_works = {
+    'today': ['Make a compliment to a friend', 'Call your grandparents', 'Embrace parents'], 'tomorrow': ['Feed the birds in the park', 'Give unnecessary things to those who need them', 'Smile']}
+
+# ['Make a compliment to a friend', 'Call your grandparents', 'Embrace parents']
+print(good_works.get('today'))
+
+# ['Feed the birds in the park', 'Give unnecessary things to those who need them', 'Smile']
+print(good_works.get('tomorrow'))
